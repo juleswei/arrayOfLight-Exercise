1630a629a29cab79b694b1ad905d87395ebc1e8b
Jules Wei <s.jules2000@gmail.com>
2016-04-04 20:30:23 +0000

    added html file for function arrayOfLight

---
+++ arrayOfLight.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Array of Light</title>
+</head>
+
+<body>
+  <p>Function: Array of Light</p>
+  <script src="arrayoflight.js"></script>
+
+
+</body>
+</html>
